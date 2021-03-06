68f40ae0cf891800cf29916f218871e3b1acc59c
Panayot <panayot.atanasov@gmail.com>
2021-03-07 01:02:21 +0200

    - Development Branch

--- src/main/java/com/example/Pet/Clinic/Spring/Project/ApplicationRunner.java
+++ src/main/java/com/example/Pet/Clinic/Spring/Project/ApplicationRunner.java
@@ -10,7 +10,7 @@ public class ApplicationRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        // Testing Branches
+        // Developing Branches
 
 
     }
